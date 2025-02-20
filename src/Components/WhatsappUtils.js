9a907ae2e6f6681ef6ecbac968638bd61b184669
F-Scienza <cufa1905@gmail.com>
2025-02-20 09:48:33 -0300

    mensaje

--- src/Components/WhatsappUtils.js
+++ src/Components/WhatsappUtils.js
@@ -2,12 +2,8 @@ const sendWsp = formData => {
 	return new Promise((resolve) => {
 
 		const phoneNumber = '3513518772'; // Número de WhatsApp al que quieres enviar el mensaje
-		const message = `Hola, mi nombre es ${formData.name}. 
-                        Estoy interesado en los servicios de ${formData.services} 
-                        para mi vehículo ${formData.vehicle}.
-                        Me gustaria que fuera ${formData.parsedDates}.
-                        ${formData.message?formData.message:''}
-                        `;
+		const message = `Hola, mi nombre es ${formData.name}. Estoy interesado en los servicios de ${formData.services} para mi vehículo ${formData.vehicle}. Me gustaria que fuera ${formData.parsedDates}. 
+		${formData.message?formData.message:''}`;
 
 		const wspURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
 			message
